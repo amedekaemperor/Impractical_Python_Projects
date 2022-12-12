8b4d644559735e692bb28635950665096caf87aa
amedekaemperor <amedekaemperor@gmail.com>
2022-12-12 11:13:39 +0000

    Create pseudonyms.py

---
+++ pseudonyms.py
@@ -0,0 +1,43 @@
+import sys, random
+from colorama import Fore
+
+def main():
+
+    print('Welcome to the Psych Sidekick Name Generator.\n')
+
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie­Weenie'",
+    "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ", 'Butterbean', 
+    'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield', 'Chewy', 'Chigger", "Cinnabuns', 
+    'Cleet', 'Cornbread', 'Crab Meat', 'Crapps', 'Dark Skies', 'Dennis Clawhammer', 
+    'Dicman', 'Elphonso', 'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+    'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny', 'Lemongrass', 'Lil Debil', 
+    'Longbranch', '"Lunch Money"', 'Mergatroid', '"Mr Peabody"', 'Oil­Can', 'Oinks', 'Old Scratch', 
+    'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet','Rock Candy', 'Schlomo', 
+    'Scratchensniff', 'Scut', "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs', 'Snorki', 
+    'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky', 'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+    "Winston 'Jazz Hands'", 'Worms')
+
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom', 'Breedslovetrout', 'Butterbaugh', 
+    'Clovenhoof', 'Clutterbuck', 'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith', 
+    'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater', 'Hootkins', 'Jefferson', 'Jenkins', 
+    'Jingley­Schmidt', 'Johnson', 'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles', 
+    'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf', 'Oxhandler', 'Pealike', 'Pennywhistle', 
+    'Peterson', 'Pieplow', 'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+    'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern', 'Stevens', 'Stroganoff', 
+    'Sugar­Gold', 'Swackhamer', 'Tippins', 'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 
+    'Weewax', 'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn', 'Woolysocks')
+
+
+    while True:
+        first_name = random.choice(first)
+        last_name = random.choice(last)
+
+        print("\n\n")
+        print(Fore.RED+f"{first_name} {last_name}", file=sys.stderr)
+
+        replay = input(Fore.WHITE+"Hit 'Q-key' to Quit or 'Enter-key' to Replay: ")
+        if replay.lower() == 'q':
+            break
+
+if __name__ == "__main__":
+    main()
